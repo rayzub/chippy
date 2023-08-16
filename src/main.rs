7e7b31fb84152a788b53b6e1808cee9b89d4ff82
0x15d3f <rayhan@0x15.dev>
2023-08-16 01:22:58 +0100

    add more roms, implement keyboard, implement frame rate with 4 ticks per frame executing 15 instructions per tick (seems stable)

--- src/main.rs
+++ src/main.rs
@@ -8,8 +8,11 @@ use interpreter::Interpreter;
 use clap::{Arg, Command};
 use sdl2::event::Event;
 use sdl2::keyboard::Keycode;
+use std::time::{Duration, SystemTime, Instant};
 
 pub const START_MEM_LOC: usize = 0x200; // 512th index
+const FRAME_RATE: Duration = Duration::from_millis(16);
+const IX_RATE: Duration = Duration::from_millis(4);
 
 
 pub struct Config { 
@@ -48,28 +51,42 @@ fn main() {
 
     // Load ROM program to memory
     interpreter.load_program(&file_contents);
-
+    let mut last_ix = Instant::now();
+    let mut last_frame = Instant::now();
     // Initiate loop to execute instructions
     'main: loop {
         // Execute single instruction
         for event in event_pump.poll_iter() {
             match event {
-                Event::KeyDown { keycode, .. } | Event::KeyUp { keycode, ..} => {
-                    if keycode == Some(Keycode::Escape) {
-                        break 'main
-                    }
-
-                    // Sets key on/off in keyboard array
-                    interpreter.bus.toggle_key(0);
-                },
-                Event::Quit { .. } => { 
-                    break 'main;
-                }
+                Event::KeyDown { keycode: Some(Keycode::Num1), .. } | Event::KeyUp { keycode: Some(Keycode::Num1), .. } => interpreter.bus.toggle_key(0),
+                Event::KeyDown { keycode: Some(Keycode::Num2), .. } | Event::KeyUp { keycode: Some(Keycode::Num2), .. } => interpreter.bus.toggle_key(1),
+                Event::KeyDown { keycode: Some(Keycode::Num3), .. } | Event::KeyUp { keycode: Some(Keycode::Num3), .. } => interpreter.bus.toggle_key(2),
+                Event::KeyDown { keycode: Some(Keycode::Num4), .. } | Event::KeyUp { keycode: Some(Keycode::Num4), .. } => interpreter.bus.toggle_key(3),
+                Event::KeyDown { keycode: Some(Keycode::Q), .. } | Event::KeyUp { keycode: Some(Keycode::Q), .. } => interpreter.bus.toggle_key(4),
+                Event::KeyDown { keycode: Some(Keycode::W), .. } | Event::KeyUp { keycode: Some(Keycode::W), .. } => interpreter.bus.toggle_key(5),
+                Event::KeyDown { keycode: Some(Keycode::E), .. } | Event::KeyUp { keycode: Some(Keycode::E), .. } => interpreter.bus.toggle_key(6),
+                Event::KeyDown { keycode: Some(Keycode::R), .. } | Event::KeyUp { keycode: Some(Keycode::R), .. } => interpreter.bus.toggle_key(7),
+                Event::KeyDown { keycode: Some(Keycode::A), .. } | Event::KeyUp { keycode: Some(Keycode::A), .. } => interpreter.bus.toggle_key(8),
+                Event::KeyDown { keycode: Some(Keycode::S), .. } | Event::KeyUp { keycode: Some(Keycode::S), .. } => interpreter.bus.toggle_key(9),
+                Event::KeyDown { keycode: Some(Keycode::D), .. } | Event::KeyUp { keycode: Some(Keycode::D), .. } => interpreter.bus.toggle_key(10),
+                Event::KeyDown { keycode: Some(Keycode::F), .. } | Event::KeyUp { keycode: Some(Keycode::F), .. } => interpreter.bus.toggle_key(11),
+                Event::KeyDown { keycode: Some(Keycode::Z), .. } | Event::KeyUp { keycode: Some(Keycode::Z), .. } => interpreter.bus.toggle_key(12),
+                Event::KeyDown { keycode: Some(Keycode::X), .. } | Event::KeyUp { keycode: Some(Keycode::X), .. } => interpreter.bus.toggle_key(13),
+                Event::KeyDown { keycode: Some(Keycode::C), .. } | Event::KeyUp { keycode: Some(Keycode::C), .. } => interpreter.bus.toggle_key(14),
+                Event::KeyDown { keycode: Some(Keycode::V), .. } | Event::KeyUp { keycode: Some(Keycode::V), .. } => interpreter.bus.toggle_key(15),
+                Event::Quit { .. } | Event::KeyDown { keycode: Some(Keycode::Escape), .. } => break 'main,
                 _ => {}
             }
         }
 
-        interpreter.tick();
-        interpreter.bus.display.draw();
+        if last_ix.elapsed() >= IX_RATE {
+            interpreter.tick();
+            last_ix = Instant::now();
+        }
+
+        if last_frame.elapsed() >= FRAME_RATE { 
+            interpreter.bus.display.draw();
+            last_frame = Instant::now();
+        }
     }
 }
